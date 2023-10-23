751ee38fbf894b0ddcb29cc1371f7c51b20046b9
panliu0310 <liuhoipan@gmail.com>
2023-10-23 14:34:59 +0800

    Restructure source folders;

---
+++ lib/page/favorites_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:life_diary/main.dart';
+import 'package:provider/provider.dart';
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text('You have '
+              '${appState.favorites.length} favourites:',
+          ),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          )
+      ],
+    );
+  }
+}
